133fcffbac2f9aead91f3fd55c6c8649c2d806b2
Neil A. Kumar <62001481+shakrunk@users.noreply.github.com>
2022-05-12 21:52:25 -0600

    Accounting for address bar in viewport height

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,11 @@ function openMenu(menu) {
     // Trigger Button Animation
     menu.classList.toggle("change");
 }
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+    // Get the viewport height and calc 'vh' unit
+    let vh = window.innerHeight * 0.01; 
+    // Set vh
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
+})
