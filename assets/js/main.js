d94f6d4bc7f3cde00058e16fce2e24d0f8822e5c
Neil A. Kumar <62001481+shakrunk@users.noreply.github.com>
2023-07-04 15:48:46 -0600

    Updated mobile nav bar to be svg

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,7 @@
 // ----- ON LOAD ----- //
-// Get the viewport height and calc 'vh' unit
-let vh = window.innerHeight * 0.01; 
-// Set vh
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+document.addEventListener("DOMContentLoaded", function() {
+    window.dispatchEvent(new Event('resize'));
+});
 
 // ----- FUNCTIONS ----- //
 // Menu Button Clicked
@@ -26,7 +25,24 @@ function openMenu(menu) {
 // We listen to the resize event
 window.addEventListener('resize', () => {
     // Get the viewport height and calc 'vh' unit
-    let vh = window.innerHeight * 0.01; 
+    let vh = window.innerHeight * 0.01;
+
     // Set vh
     document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+    // Get the height of the mobile nav element
+    let svgElement = document.getElementById("mobileNavSVG");
+    let totalHeight = svgElement.getBoundingClientRect().height;
+
+    // Set the viewbox of the svg to span the width of the screen and have a height of 55px
+    document.getElementById("mobileNavSVG").setAttribute('viewBox',`0 0 ${window.innerWidth} ${totalHeight}`);
+
+    // Set mobile nav properties based on viewport
+    let curveHeight = totalHeight - 15; // Stop 15 pixels before the bottom
+    let buttonWidth = curveHeight; // make the button a square
+    let curveWidth = curveHeight; // 40 pixels
+    let betweenWidth = window.innerWidth - (2*buttonWidth) - (2*curveWidth); // Remaining pixels between buttons
+
+    // Create SVG path based on viewport
+    document.getElementById("mypath").setAttribute("d",`M 0 0 l ${buttonWidth} 0 c ${curveWidth/2} 0 ${curveWidth/2} ${curveHeight} ${curveWidth} ${curveHeight} l ${betweenWidth} 0 c ${curveWidth/2} 0 ${curveWidth/2} -${curveHeight} ${curveWidth} -${curveHeight} l ${buttonWidth} 0 l 0 ${totalHeight} L 0 ${totalHeight} L 0 0`);
 })
